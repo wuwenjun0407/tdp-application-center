e8cbeecf4d56cb08e0f6ef0a5cb076f6b1fcf75d
wuwj8 <wuwj8@lenovo.com>
2022-03-07 11:20:58 +0800

    feat(*): git hooks(1)

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // 'src/**/*{js,jsx,ts,tsx,md,html}': ['npm run lint:ts'],
+    'src/**/*{js,jsx,ts,tsx,md,html}': ['lint:eslint']
     // 'src/**/*.less': ['npm run lint:style', 'git add'],
     // '*': ['pretty-quick --staged']
 };

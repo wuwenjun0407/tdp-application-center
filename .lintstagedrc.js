06c88f84f75d4fe0c60f4ef81b58377a25e4d151
wuwj8 <wuwj8@lenovo.com>
2022-03-07 11:09:41 +0800

    feat(*): 配置git hooks

---
+++ .lintstagedrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // 'src/**/*{js,jsx,ts,tsx,md,html}': ['npm run lint:ts'],
+    // 'src/**/*.less': ['npm run lint:style', 'git add'],
+    // '*': ['pretty-quick --staged']
+};
